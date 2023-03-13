653276ddc613485272f463ccca7ffa19f577e0a7
Leonardo dos Santos <leodoima@hotmail.com>
2023-03-13 08:56:40 -0300

    Incrementando recursos de UPDATE e DELETE da classe Book

--- src/main/java/com/controller/BookController.java
+++ src/main/java/com/controller/BookController.java
@@ -4,10 +4,10 @@ import com.model.Book;
 import com.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/books")
@@ -23,9 +23,25 @@ public class BookController {
         return bookRepository.findAll();
     }
 
+    @RequestMapping(value = "/{id}")
+    public Optional<Book> findById(@PathVariable Long id) {
+        return bookRepository.findById(id);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@RequestBody Book book) {
         return bookRepository.save(book);
+    }
+
+    @PutMapping
+    public Book update(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        bookRepository.deleteById(id);
     }
 }
